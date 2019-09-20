bd5fc87cf9ee9409db0488375184780ab2669615
Josh Rowley <joshua.f.rowley@gmail.com>
2019-09-20 00:06:28 -0400

    better approach

--- random.rb
+++ random.rb
@@ -1,12 +1,14 @@
 require 'pry'
 
-session_list = [
-  [1, 3, 2],
-  [1, 4, 2],
-  [5, 4, 3],
-  [5, 2, 3],
-  [5, 1, 4]
+sessions = [
+  ["A", "B", "E"],
+  ["A", "B", "D"],
+  ["A", "C", "D"],
+  ["B", "C", "E"],
+  ["C", "D", "E"]
 ]
+
+
 
 participant_list = [
   "Janice Bad Moccasin",
@@ -59,63 +61,46 @@ participant_list = [
 ]
 
 class Session
-  attr_reader :id, :time, :participants
+  attr_reader :topic, :time, :participants
 
-  def self.from_list(list)
-    list.each_with_index.map do |session, index|
-      session.map do |id|
-        new(id, index)
-      end
-    end
-  end
-
-  def initialize(id, time)
-    @id = id
+  def initialize(topic, time)
+    @topic = topic
     @time = time
     @participants = []
   end
 
-  def name
-    "#{time} #{facilitator}"
+  def register(participant)
+    @participants.push(participant)
   end
+end
 
-  def facilitator
-    case @id
-    when 1
-      "Adam Horowitz / Gabrielle Uballez"
-    when 2
-      "Pamela Villesenor"
-    when 3
-      "Autumn White Eyes"
-    when 4
-      "Halima Cassells"
-    when 5
-      "Marlene Cancio Ramirez"
+class SessionTopic
+  attr_reader :facilitator, :sessions
+  
+  FACILITATORS = [
+    "Adam Horowitz / Gabrielle Uballez",
+    "Pamela Villesenor",
+    "Autumn White Eyes",
+    "Halima Cassells",
+    "Marlene Cancio Ramirez"
+  ]
+
+  def self.from_list(list)
+    list.each_with_index.map do |sessions, i|
+      new(FACILITATORS[i], sessions)
     end
   end
 
-  def time
-    case @time
-    when 0
-      "A"
-    when 1
-      "B"
-    when 2
-      "C"
-    when 3
-      "D"
-    when 4
-      "E"
+  def initialize(facilitator, sessions)
+    @facilitator = facilitator
+    @sessions = sessions.map do |time|
+      Session.new(self, time)
     end
-  end
-
-  def add_participant(participant)
-    @participants << participant
   end
 end
 
 class Participant
-  attr_reader :name, :sessions
+  attr_reader :name, :registered_sessions
 
   def self.from_list(list)
     list.map do |name|
@@ -125,97 +110,47 @@ class Participant
 
   def initialize(name)
     @name = name
-    @sessions = []
+    @registered_sessions = []
   end
 
-  def add_session(session)
-    @sessions << session
+  def allowed_sessions(topic)
+    registered_times = registered_sessions.map(&:time)
+    topic.sessions.reject do |session|
+      registered_times.include?(session.time)
+    end
   end
 
-  def can_accept_session?(session)
-    !sessions.map(&:id).include?(session.id)
+  def register(session)
+    puts "#{name} registered for #{session.time} with #{session.topic.facilitator}"
+    @registered_sessions.push(session)
+  end
+
+  def complete_registration?
+    @registered_sessions.length == 5
+  end
+
+  def reset!
+    puts "#{name} reset!"
+    @registered_sessions = []
   end
 end
 
-
+# shuffle participants
 #
-# RULES
-#
-# 1. all partipants attend all 5 sessions
-# 2. no chapter session should be the same as another
-# 3. groups should be < 20 people (balance them)
-#
+# for each participant
+# #  for each topic
+# #    which sessions are allowed for this participant?
+# #    pick the session with least amount of people
+# #    if tied, randomly pick a session
 
-class ScheduleGenerator
-  attr_reader :participants, :sessions, :schedule
-
-  def initialize(participants, sessions)
-    @participants = participants
-    @sessions = sessions
-  end
-
-  def random_participant(pluck_copy)
-  end
-
-  def random_schedule
-    puts "#{ sessions.length } sessions"
-    puts "#{ participants.length } participants"
-    puts "Creating schedule"
-
-    # each time block
-    sessions.each do |time_block|
-      pluck_copy = participants.dup.shuffle
-      
-      time_block.cycle do |session|
-
-        match = false
-        until match
-          participant = pluck_copy.shift
-          # if !participant
-            # binding.pry
-          # end
-          if participant.can_accept_session?(session)
-            participant.add_session(session)
-            session.add_participant(participant)
-            puts "Added #{participant.name} to session #{session.id} #{session.time}"
-            match = true
-          else
-            puts "Didn't match #{participant.name}, retrying..."
-            pluck_copy.push(participant)
-
-            if pluck_copy.all? { |p| !p.can_accept_session?(session) }
-              puts "skipping to next session, no participants available"
-              next
-            end
-          end
-        end
-
-        pluck_copy.compact!
-
-        if pluck_copy.length == 0
-          break
-        end
-      end
-    end
-
-    sessions.each_with_index do |seshes, i|
-      puts "\nSESSION #{seshes.first.time}\n\n\n"
-
-      seshes.each do |s|
-        puts "Session #{ s.time } #{ s.facilitator }\n\n"
-        
-        puts s.participants.map(&:name)
-        puts "\n\n"
-      end
-    end
-
-    participants.each do |p|
-      puts "\n\n #{p.name}'s schedule\n\n"
-      puts p.sessions.map(&:name)
-    end
-  end
-end
-
+topics = SessionTopic.from_list(sessions)
 participants = Participant.from_list(participant_list)
-sessions = Session.from_list(session_list)
-ScheduleGenerator.new(participants, sessions).random_schedule
+
+participants.shuffle.each do |participant|
+  until participant.complete_registration?
+    topics.each do |topic|
+      allowed = participant.allowed_sessions(topic)
+      allowed.length == 0 ?  participant.reset! : participant.register(allowed.sample)
+    end
+  end
+end
